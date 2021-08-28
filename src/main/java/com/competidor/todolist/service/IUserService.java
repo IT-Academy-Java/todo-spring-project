f860f86ed67f75e07d63c43391021053505fdd1c
Elena Piaggio <elenapiaggio@gmail.com>
2021-08-28 23:57:47 +0200

    ADD IUserService

--- src/main/java/com/competidor/todolist/service/IUserService.java
+++ src/main/java/com/competidor/todolist/service/IUserService.java
@@ -1,2 +1,17 @@
-package com.competidor.todolist.service;public class IUserService {
+package com.competidor.todolist.service;
+
+import com.competidor.todolist.entity.User;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface IUserService {
+
+    public User save(User user);
+    public List<User> getUsers();
+    public Optional<User> getUserById(String userId);
+    public Boolean deleteUserById(String userId);
+    public Boolean update(User user,String userId);
+
 }
