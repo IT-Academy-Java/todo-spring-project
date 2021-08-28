655ea51cf525bfc17f3870cf5c3541fa7aaf16b4
Elena Piaggio <elenapiaggio@gmail.com>
2021-08-28 23:59:08 +0200

    ADD user controller

--- src/main/java/com/competidor/todolist/controller/UserController.java
+++ src/main/java/com/competidor/todolist/controller/UserController.java
@@ -1,2 +1,37 @@
-package com.competidor.todolist.controller;public class UserController {
+package com.competidor.todolist.controller;
+
+import com.competidor.todolist.entity.User;
+import com.competidor.todolist.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+    @GetMapping()
+    public ResponseEntity<List<User>> getAll() {
+        return new ResponseEntity<>(userService.getUsers(), HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> save(@RequestBody User user){
+        return new ResponseEntity<>(userService.save(user), HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<User> getUserById(@PathVariable("id") String userId){
+        return userService.getUserById(userId)
+                .map(user -> new ResponseEntity<>(user, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
 }
