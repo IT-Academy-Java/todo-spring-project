c21e953630040f56e2fb92d520c31f8a765b2aaf
Elena Piaggio <elenapiaggio@gmail.com>
2021-09-16 12:32:42 +0200

    ADD todoService implementation

--- src/main/java/com/competidor/todolist/service/impl/TodoItemServiceImpl.java
+++ src/main/java/com/competidor/todolist/service/impl/TodoItemServiceImpl.java
@@ -1,2 +1,52 @@
-package com.competidor.todolist.service.impl;public class TodoItemServiceImpl {
+package com.competidor.todolist.service.impl;
+
+
+import com.competidor.todolist.entity.TodoItem;
+import com.competidor.todolist.repository.ITodoItemRepository;
+import com.competidor.todolist.service.ITodoItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+
+@Service
+public class TodoItemServiceImpl implements ITodoItemService {
+
+  @Autowired
+  private ITodoItemRepository todoItemRepository;
+
+  @Override
+  public TodoItem save(TodoItem item) {
+    return todoItemRepository.save(item);
+  }
+
+  @Override
+  public List<TodoItem> getTodoItems() {
+    return todoItemRepository.findAll();
+  }
+
+  @Override
+  public Optional<TodoItem> getTodoItemById(String todoItemId) {
+    return todoItemRepository.getTodoItemById(todoItemId);
+  }
+
+  @Override
+  public boolean deleteTodoItemById(String todoItemId) {
+    return getTodoItemById(todoItemId).map( todoItem -> {
+      todoItemRepository.deleteById(todoItemId);
+      return true;
+    }).orElse(false);
+  }
+
+  @Override
+  public boolean update(TodoItem item) {
+    return getTodoItemById(item.getId()).map(todoItem -> {
+      todoItemRepository.save(item);
+      return true;
+    }).orElse(false);
+  }
+
+
 }
