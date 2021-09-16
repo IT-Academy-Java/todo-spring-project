91dd8b549e632321152841f6dbf43774e6f75338
Elena Piaggio <elenapiaggio@gmail.com>
2021-09-16 12:31:54 +0200

    ADD todoItem service

--- src/main/java/com/competidor/todolist/service/ITodoItemService.java
+++ src/main/java/com/competidor/todolist/service/ITodoItemService.java
@@ -1,2 +1,18 @@
-package com.competidor.todolist.service;public class ITodoItemService {
+package com.competidor.todolist.service;
+
+import com.competidor.todolist.entity.TodoItem;
+
+import java.util.List;
+import java.util.Optional;
+
+
+public interface ITodoItemService {
+
+  public TodoItem save(TodoItem item);
+  public List<TodoItem> getTodoItems();
+  public Optional<TodoItem> getTodoItemById(String todoItemId);
+  public boolean deleteTodoItemById(String todoItemId);
+  public boolean update(TodoItem item);
+
 }
+
